c22d7221011a7896898c7875c795b7ad6cf32c3c
Eiden <eidnegabriel07@gmail.com>
2023-01-27 18:10:48 -0400

    fix?

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -5,6 +5,7 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
     """ Caching System """
+
     def __init__(self) -> None:
         """ Constructor and init super """
         super().__init__()
