74d86ebbbb0e2f0f9d2577ba22b045891712f513
Eiden <eidnegabriel07@gmail.com>
2023-03-07 20:42:54 -0400

    task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""" Task 0 """
+""" exercise module """
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -19,3 +19,21 @@ class Cache:
         randomKey = str(uuid.uuid4())
         self._redis.set(randomKey, data)
         return randomKey
+
+    def get(self, key: str, fn: Callable = None) -> Union[str,
+                                                          bytes,
+                                                          int,
+                                                          float]:
+        """ recovering original type """
+        data = self._redis.get(key)
+        if fn:
+            fn(data)
+        return data
+
+    def get_str(self):
+        """ Parametrize Cache.get with the correct conversion function
+        """
+
+    def get_int(self):
+        """ Parametrize Cache.get with the correct conversion function
+        """
