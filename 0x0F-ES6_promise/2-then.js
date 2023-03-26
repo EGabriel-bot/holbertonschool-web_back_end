2b1be6feae905a3eb69ad9cc7a684ecfdb1ee3c1
Eiden <eidnegabriel07@gmail.com>
2023-03-25 21:20:51 -0400

    task 2

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,10 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(
-    () => {
-      const resolve = { status: 200, body: 'success' };
-      console.log('Got a response from the api');
-      return resolve;
-    },
+    () => ({ status: 200, body: 'success' }),
     (reject) => reject(new Error()),
-  );
+  )
+    .finally(() => console.log('Got a response from the api'));
 }
